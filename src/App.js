f19143606df01578804866bbffdfe10cc963a0bd
jjade seravla <jadealvares@hotmail.co.uk>
2018-09-12 13:49:26 +0100

    using axios to fetch API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
-import { BroswerRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
+
+import { Provider } from './context';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-      <React.Fragment>
-        <Navbar/>
-        <div className="conainer">
-          <Switch>
-            <Route exact path="/" component={Index} />
-          </Switch>
-        </div>
-      </React.Fragment>
-    </Router>
+      <Provider>
+        <Router>
+          <React.Fragment>
+          <Navbar/>
+          <div className="conainer">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
+  </Provider>
     );
   }
 }
