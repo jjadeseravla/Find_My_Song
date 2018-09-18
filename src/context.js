22a014790f0060bc228827f99f34c0e320daed1f
jjade seravla <jadealvares@hotmail.co.uk>
2018-09-18 16:23:38 +0100

    search bar now shows song titles (useing redux) and setsState to empty to clean search box when press enter

--- src/context.js
+++ src/context.js
@@ -3,10 +3,26 @@ import axios from 'axios';
 
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+  switch(action.type) { //evaluate action type
+    case 'SEARCH_TRACKS':
+    return {
+      ...state,
+      track_list: action.payload,
+      heading: 'Search Results'
+    };
+    default:
+      return state;
+  }
+}
+
 export class Provider extends Component {
   state = {
     track_list: [],
-    heading: 'Top 10 Tracks'
+    heading: 'Top 10 Tracks',
+    dispatch: action => this.setState(state => reducer(state, action))
+
+    //allows to have a reducer where we can call this dispatch to any consumer component and do what we want, eg.replace 10 tracks with what we have searched
   };
 
   componentDidMount() {
