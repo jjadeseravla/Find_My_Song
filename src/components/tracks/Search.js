50c3fb0c2f7413a410a8cc6d64dcafbf800b3711
jjade seravla <jadealvares@hotmail.co.uk>
2018-09-18 14:29:58 +0100

    every key in input changes the state of trackTitle

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -7,6 +7,10 @@ state = {
   trackTitle: ''
 }
 
+onChange(e) {
+    this.setState({ [e.target.name]: e.target.value }); //make sure trackTitle (which is a set key) matches your name on line 30
+}
+
   render() {
     return (
       <Consumer>
@@ -15,8 +19,20 @@ state = {
             <div className="card card-body mb-4 p-4">
               <h1 className="display-4 text-center">
                 <i className="fas fa-music"> Search for a Song</i>
-                <p className="lead text-center">Get the Lyrics for any Song</p>
               </h1>
+              <p className="lead text-center">Get the Lyrics for any Song</p>
+              <form>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Song title..."
+                    name="trackTitle"
+                    value={this.state.trackTitle}
+                    onChange={this.onChange.bind(this)}
+                  />
+                </div>
+              </form>
             </div>
           );
         }}
